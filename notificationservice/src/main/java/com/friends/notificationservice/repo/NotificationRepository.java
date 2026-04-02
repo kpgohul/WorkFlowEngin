07468aaf1a9a6129746da8078bccb11ed33bbc95
Gohul K <144474239+kpgohul@users.noreply.github.com>
2026-04-02 22:16:51 +0530

    Delete notificationservice directory
    Not need as we planned to implement a generic action service

--- notificationservice/src/main/java/com/friends/notificationservice/repo/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.friends.notificationservice.repo;
-
-import com.friends.notificationservice.entity.NotificationLog;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import java.util.UUID;
-
-public interface NotificationRepository extends ReactiveCrudRepository<NotificationLog, UUID> {
-}
